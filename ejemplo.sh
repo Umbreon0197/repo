e420bde09bd2b29c05f4e999b56027ac09ede621
Javi Portero <146830532+Umbreon0197@users.noreply.github.com>
2025-11-14 20:01:14 +0100

    hola

--- ejemplo.sh
+++ ejemplo.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-echo="Validando estructuro JSON..."
+echo "Validando estructura JSON..."
 cat data.json | jq empty
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
+ echo "7 JSON válido"
+else
+ echo " JSON con errores"
+ exit 1
+fi
